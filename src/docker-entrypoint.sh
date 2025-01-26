d4971945868c51c3e8fe5f2d5af12649a1c1641a
Mihhail Matvejev <m4tveevm@icloud.com>
2025-01-26 17:42:53 +0300

    fix docker CI/CD with adding waiting for database start

--- src/docker-entrypoint.sh
+++ src/docker-entrypoint.sh
@@ -1,5 +1,13 @@
 #!/bin/sh
-set -e
 
+echo "Waiting for database..."
+until pg_isready -h db -U postgres; do
+  echo "Waiting for PostgreSQL to be ready..."
+  sleep 3
+done
+
+echo "Database is up - running migrations."
+python timetable/manage.py migrate
+python timetable/manage.py collectstatic --noinput
 
 exec "$@"
