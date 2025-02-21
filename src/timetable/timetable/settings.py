f3c560a384009e47401263b88ef92ac066ce1a4c
Mihhail Matvejev <m4tveevm@icloud.com>
2025-02-21 13:45:20 +0300

    Refactoring: setting default status False and removing it from stack.yml unness line
    Signed-off-by: Mihhail Matvejev <m4tveevm@icloud.com>

--- src/timetable/timetable/settings.py
+++ src/timetable/timetable/settings.py
@@ -10,7 +10,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = config("DJANGO_SECRET", default="fallback_secret_key")
 
-DEBUG = strtobool(config("DEBUG", default="False"))
+DEBUG = config("DEBUG", default="False", cast=lambda v: bool(strtobool(v or "False")))
 
 ALLOWED_HOSTS = config("DJANGO_ALLOWED_HOSTS", default="*", cast=Csv())
 
