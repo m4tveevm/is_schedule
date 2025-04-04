a15931b9c4369f84a46f485771297240c459312b
Mihhail Matvejev <m4tveevm@updspace.com>
2025-04-04 20:46:44 +0300

    Refactoring: updating CI and making auto user creation
    Signed-off-by: Mihhail Matvejev <m4tveevm@updspace.com>

--- src/docker-entrypoint.sh
+++ src/docker-entrypoint.sh
@@ -11,4 +11,17 @@ echo "Database is up - running migrations."
 python manage.py migrate --noinput
 python manage.py collectstatic --noinput
 
+python manage.py shell <<EOF
+import os
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+username = os.environ.get('DJANGO_SUPERUSER_USERNAME')
+if not User.objects.filter(username=username).exists():
+    User.objects.create_superuser(
+        username=username,
+        email=os.environ.get('DJANGO_SUPERUSER_EMAIL'),
+        password=os.environ.get('DJANGO_SUPERUSER_PASSWORD')
+    )
+EOF
 exec "$@"
